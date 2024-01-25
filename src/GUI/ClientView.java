e8893df475445cd2770356302e0363a99888fc14
Ivan Abrami <Ivanpitbull2512@gmail.com>
2024-01-25 21:57:02 +0100

    Background color for section moved inside constructor, also all sections now have the same color. Sections now have titles

--- src/GUI/ClientView.java
+++ src/GUI/ClientView.java
@@ -23,18 +23,8 @@ public class ClientView extends JFrame {
         JPanel portfolioAndSale = new JPanel();
         portfolioAndSale.setLayout(new GridLayout(2,1));
 
-        Section portfolio = new Section();
-        JPanel portfolioHeading = portfolio.getHeading();
-        JPanel portfolioContent = portfolio.getContent();
-        portfolioHeading.setBackground(Color.PINK);
-        portfolioContent.setBackground(Color.RED);
-
-
-        Section sale = new Section();
-        JPanel saleHeading = sale.getHeading();
-        JPanel saleContent = sale.getContent();
-        saleHeading.setBackground(Color.CYAN);
-        saleContent.setBackground(Color.BLUE);
+        Section portfolio = new Section("Portfolio");
+        Section sale = new Section("Buy & Sell");
 
         portfolioAndSale.add(portfolio);
         portfolioAndSale.add(sale);
@@ -42,11 +32,6 @@ public class ClientView extends JFrame {
     }
 
     private Section getPriceSection(){
-        Section prices = new Section();
-        JPanel pricesHeading = prices.getHeading();
-        JPanel pricesContent = prices.getContent();
-        pricesHeading.setBackground(Color.GRAY);
-        pricesContent.setBackground(Color.LIGHT_GRAY);
-        return prices;
+        return new Section("Prices");
     }
 }
--- src/GUI/Section.java
+++ src/GUI/Section.java
@@ -7,13 +7,19 @@ public class Section extends JPanel {
     private final JPanel heading;
     private final JPanel content;
 
-    public Section(){
+    public Section(String title){
         super();
         setLayout(new BorderLayout());
         heading = new JPanel();
+        JLabel headingTitle = new JLabel();
+        headingTitle.setFont(new Font("Times New Roman",Font.PLAIN,24));
+        headingTitle.setText(title);
+        heading.add(headingTitle);
         content = new JPanel();
         add(heading,BorderLayout.NORTH);
         add(content,BorderLayout.CENTER);
+        heading.setBackground(Color.GRAY);
+        content.setBackground(Color.LIGHT_GRAY);
     }
 
     public JPanel getHeading() {
