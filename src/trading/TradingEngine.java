b11814a0ebf85a9b53d0e7a3cf41417590fb223b
Ivan Abrami <Ivanpitbull2512@gmail.com>
2024-02-06 00:10:12 +0100

    Changed TradingEngine structur to TreeMap based and added insertOrder (only works for limit orders)

--- src/trading/TradingEngine.java
+++ src/trading/TradingEngine.java
@@ -1,15 +1,30 @@
 package trading;
 
-import java.util.Queue;
 import java.util.TreeMap;
-import java.util.TreeSet;
 
 public class TradingEngine {
-    private TreeSet<Limit> bids;
-    private TreeSet<Limit> asks;
+    private TreeMap<Integer,Limit> bids;
+    private TreeMap<Integer,Limit> asks;
 
     public TradingEngine(){
-        bids = new TreeSet<>(new BidsComparator());
-        asks = new TreeSet<>(new AsksComparator());
+        bids = new TreeMap<>((a , b) -> -Integer.compare(a,b));
+        asks = new TreeMap<>(Integer::compare);
+    }
+
+    public void insertOrder(Order order){
+        if(order.getSide() == OrderSide.BID){
+            putOrderInMap(order,bids);
+        }else{ // order.getSide() == OrderSide.ASK
+            putOrderInMap(order,asks);
+        }
+    }
+
+    private static void putOrderInMap(Order order, TreeMap<Integer,Limit> map){
+        if(map.containsKey(order.getPrice())){
+            Limit limitLevel = map.get(order.getPrice());
+            limitLevel.insertOrder(order);
+        }else{
+            map.put(order.getPrice(),new Limit(order.getPrice()));
+        }
     }
 }
